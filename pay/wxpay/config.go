b4113f01e82957708bacdcc2fa4524b8d9186eef
zhangboyu <zhangjunfang0505@163.com>
2017-07-28 14:10:52 +0800

    20170728141052

---
+++ pay/wxpay/config.go
@@ -0,0 +1,11 @@
+package wxpay
+
+type WxConfig struct {
+	AppId         string
+	AppKey        string
+	MchId         string
+	NotifyUrl     string
+	PlaceOrderUrl string
+	QueryOrderUrl string
+	TradeType     string
+}
